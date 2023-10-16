6662c8f60c47dcfe07c4eca04f8a3b73af6eed20
Khurshed Alam <khurshedalam400@gmail.com>
2023-10-16 19:20:33 +0600

    Assign Super admin in a project

---
+++ src/main/java/base/AssignUserPage.java
@@ -0,0 +1,45 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AssignUserPage {
+
+    public WebDriver driver;
+
+    public AssignUserPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public WebElement AssignUserBtn(){
+        return driver.findElement(By.xpath("//span[contains(text(),'Assign Users')]"));
+    }
+
+    public void goToAssignUser(){
+        AssignUserBtn().click();
+
+    }
+
+    public WebElement AssignUserModal(){
+        return driver.findElement(By.xpath("//div[@id='react-select-5-placeholder' and contains(text(),'Enter a User name')]"));
+    }
+
+    public WebElement AddClient(){
+        return driver.findElement(By.xpath("//button[contains(text(),'Add to Project')]") );
+    }
+
+    public void goToAddClient() throws InterruptedException {
+
+       AssignUserBtn().click();
+       Thread.sleep(5000);
+       //AssignUserModal().sendKeys(Keys.ENTER);
+       //Thread.sleep(2000);
+       AssignUserModal().sendKeys("Client");
+       Thread.sleep(5000);
+       AssignUserModal().sendKeys(Keys.ENTER);
+       Thread.sleep(2000);
+        AddClient().click();
+    }
+}
